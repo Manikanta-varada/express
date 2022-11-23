7ff5e59ecdb55c8451e3971f81551420483b2b7e
jahnavichanapathi <chanapathijahnavi@gmail.com>
2022-11-23 17:17:41 +0530

    express

---
+++ data.js
@@ -0,0 +1,7 @@
+const students=[
+{id:1,stdname:"arjun",stdclass:"cse"},
+{id:2,stdname:"manikanta",stdclass:"eee"},
+{id:3,stdname:"arjun",stdclass:"civil"},
+{id:4,stdname:"charan",stdclass:"ca"}]
+
+module.exports=students
